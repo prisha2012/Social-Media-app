e43c30ba4450209725fccb6ee914678c1be66d02
Prisha <153307711+prisha2012@users.noreply.github.com>
2025-07-10 23:29:15 +0530

    chore: remove deprecated mongoose options and update project

--- backend1/seedCelebrities.js
+++ backend1/seedCelebrities.js
@@ -55,7 +55,7 @@ const celebrities = [
 ];
 
 async function seed() {
-  await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  await mongoose.connect(process.env.MONGO_URI)
   for (const celeb of celebrities) {
     let user = await User.findOne({ email: celeb.email });
     if (!user) {
